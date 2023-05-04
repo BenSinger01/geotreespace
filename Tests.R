2b9994d6c7f2d085670144ab9a72ecb2b7e9336d
iDNA78 <38106241+iDNA78@users.noreply.github.com>
2023-05-04 10:25:32 +0100

    Remove package loading

--- Tests.R
+++ Tests.R
@@ -2,11 +2,11 @@
 # create some test objects
 ############################
 
-tree_a <- read.beast(testthat::test_path("examples", "mtGeo.MCC.txt"))
-tree_b <- read.beast(testthat::test_path("examples", "yGeo.MCC.txt"))
-tree_c <- read.beast(testthat::test_path("examples", "autoGeo.MCC.txt"))
+tree_a <- read.beast(test_path("examples", "mtGeo.MCC.txt"))
+tree_b <- read.beast(test_path("examples", "yGeo.MCC.txt"))
+tree_c <- read.beast(test_path("examples", "autoGeo.MCC.txt"))
 
-trees <- c(tree_a,tree_b,tree_c)
+trees <- c(tree_a, tree_b, tree_c)
 class(trees) <- "multiPhylo"
 
 ############################
@@ -22,15 +22,15 @@ test_that("mrcaDist not sensitive to permuting inputs", {
 	})
 
 test_that("mrcaDist same when computed from mrcaVec", {
-	expect_equal(mrcaDist(tree_a,tree_b, coord_name="geo", continuous=TRUE),mrcaDist(mrcaVec(tree_a, coord_name="geo", continuous=TRUE),mrcaVec(tree_b, coord_name="geo", continuous=TRUE), coord_name="geo", continuous=TRUE))
+	expect_equal(mrcaDist(tree_a, tree_b, coord_name="geo", continuous=TRUE),mrcaDist(mrcaVec(tree_a, coord_name="geo", continuous=TRUE), mrcaVec(tree_b, coord_name="geo", continuous=TRUE), coord_name="geo", continuous=TRUE))
 	})
 
 test_that("save_memory version of multiMrcaDist equals normal multiMrcaDist", {
-	expect_equal(multiMrcaDist(trees, coord_name="geo", continuous=TRUE),multiMrcaDist(trees, coord_name="geo", continuous=TRUE,save.memory=TRUE))
+	expect_equal(multiMrcaDist(trees, coord_name="geo", continuous=TRUE), multiMrcaDist(trees, coord_name="geo", continuous=TRUE, save.memory=TRUE))
 	})
 
 test_that("multiMrcaDist of mtGeo, yGeo, and autoGeo is correct", {
-	expect_equal(multiMrcaDist(trees,coord_name="geo",continuous=TRUE)[1:3],c(6.118718447700729257122,4.894385071111685192591,4.555614265727615297408))
+	expect_equal(multiMrcaDist(trees, coord_name="geo", continuous=TRUE)[1:3], c(6.118718447700729257122, 4.894385071111685192591, 4.555614265727615297408))
 	})
 
 ############################
@@ -38,15 +38,15 @@ test_that("multiMrcaDist of mtGeo, yGeo, and autoGeo is correct", {
 ############################
 
 test_that("maspgDist of mtGeo and yGeo is correct", {
-	expect_equal(maspgDist(tree_a,tree_b, coord_name="geo", discretization=1)[1],0.875)
+	expect_equal(maspgDist(tree_a, tree_b, coord_name="geo", discretization=1)[1], 0.875)
 	})
 
 test_that("maspgDist not sensitive to permuting inputs", {
-	expect_equal(maspgDist(tree_a,tree_b, coord_name="geo", discretization=1),maspgDist(tree_b,tree_a, coord_name="geo", discretization=1))
+	expect_equal(maspgDist(tree_a, tree_b, coord_name="geo", discretization=1), maspgDist(tree_b, tree_a, coord_name="geo", discretization=1))
 	})
 
 test_that("multiMaspgDist of mtGeo, yGeo, and autoGeo is correct", {
-	expect_equal(multiMaspgDist(trees, coord_name="geo", discretization=1)[1:3],c(0.875,0.750,0.875))
+	expect_equal(multiMaspgDist(trees, coord_name="geo", discretization=1)[1:3], c(0.875, 0.750, 0.875))
 	})
 
 ############################
@@ -65,6 +65,6 @@ test_that("error is given if input is not of class phylo / multiphylo", {
 test_that("error is given if trees have different tip labels", {
   tree_d <- tree_a
   tree_d@phylo$tip.label <- 1:16
-  expect_error(mrcaDist(tree_a,tree_d))
-  expect_error(maspgDist(tree_a,tree_d))
+  expect_error(mrcaDist(tree_a, tree_d))
+  expect_error(maspgDist(tree_a, tree_d))
   })
